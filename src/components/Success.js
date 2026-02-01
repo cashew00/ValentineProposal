1bb68312c9b5f586a2381816b7f6b1f37426eee5
cashew00 <aswathy27600@gmail.com>
2026-02-02 02:03:48 +0530

    Update Success.js

--- src/components/Success.js
+++ src/components/Success.js
@@ -12,11 +12,10 @@ const Success = () => (
     <img className="App-gif" src={kisses} alt="Kisses" />
     {/* Personalized success messages */}
     <p className="App-text-success">
-      You're my favorite movie buddy, my princess, and the one I enjoy every
-      moment with.
+      Yasssssss hehe. I loveee youuuuu I miss youuuuu
     </p>
     <p className="App-text-success">
-      Looking forward to spending Valentine's Day with you, Jocelyn.
+      Come meet me fast.Need kissies to stay happy.
     </p>
     {/* Displaying the date of the special moment */}
     <p className="App-text-date">12/23/2023</p>
